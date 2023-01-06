7c6dabb4c5089d1cf82acd2ed86dc15a643ffdf3
Philip Nguyen <philipnguyen953@gmail.com>
2023-01-05 16:38:27 -0800

    Added Google and Github Auth with AuthJS

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
   return (
@@ -14,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-
+      <button onClick={() => signIn()}>Login</button>
       </main>
     </>
   )
