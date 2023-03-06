7ad5aed6ab026e579b78f95a5cd0592dbaf96619
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-06 13:21:17 -0800

    Added MobileFooter to Index

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Feed from "../components/Feed";
 import Modal from "../components/Modal";
+import MobileFooter from "../components/MobileFooter";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -47,6 +48,7 @@ export default function Home() {
                 fetchAPI={fetchAPI}
                 setFetchAPI={setFetchAPI}
             />
+            <MobileFooter />
         </div>
     );
 }
