1200b85e31c1b6f9674368c77976bf5e4853ca40
Philip Nguyen <philipnguyen953@gmail.com>
2023-03-06 13:32:46 -0800

    Hide when no user session is available

--- components/MobileFooter.js
+++ components/MobileFooter.js
@@ -12,38 +12,39 @@ const MobileFooter = () => {
     const [openModal, setOpenModal] = useRecoilState(modalState);
 
     return (
-        <div className="fixed left-0 bottom-0 w-full bg-gray-100 py-2 px-8 md:hidden">
-            <div className="flex w-full justify-between items-center">
-                <Link href="/">
-                    <AiFillHome className="navBtn" size={24} />
-                </Link>
-                {session ? (
-                    <>
-                        <div className="relative">
-                            <BiPaperPlane className="navBtn" size={24} />
-                            <div className="absolute -top-2 -right-2 text-xs w-5 h-5 text-white bg-red-500 rounded-full flex items-center justify-center animate-pulse">
-                                3
-                            </div>
-                        </div>
-                        <BiPlusCircle
-                            onClick={() => setOpenModal(true)}
-                            className="navBtn"
-                            size={24}
-                        />
-                        <HiOutlineUserGroup className="navBtn" size={24} />
-                        <AiOutlineHeart className="navBtn" size={24} />
+        <div>
+            {session && (
+                <div>
+                    <div className="fixed left-0 bottom-0 w-full bg-gray-100 py-2 px-8 md:hidden">
+                        <div className="flex w-full justify-between items-center">
+                            <Link href="/">
+                                <AiFillHome className="navBtn" size={24} />
+                            </Link>
 
-                        <img
-                            className="h-8 rounded-full cursor-pointer"
-                            src={session?.user?.image}
-                            alt="profile picture"
-                            onClick={signOut}
-                        />
-                    </>
-                ) : (
-                    <button onClick={signIn}>Sign In</button>
-                )}
-            </div>
+                            <div className="relative">
+                                <BiPaperPlane className="navBtn" size={24} />
+                                <div className="absolute -top-2 -right-2 text-xs w-5 h-5 text-white bg-red-500 rounded-full flex items-center justify-center animate-pulse">
+                                    3
+                                </div>
+                            </div>
+                            <BiPlusCircle
+                                onClick={() => setOpenModal(true)}
+                                className="navBtn"
+                                size={24}
+                            />
+                            <HiOutlineUserGroup className="navBtn" size={24} />
+                            <AiOutlineHeart className="navBtn" size={24} />
+
+                            <img
+                                className="h-8 rounded-full cursor-pointer"
+                                src={session?.user?.image}
+                                alt="profile picture"
+                                onClick={signOut}
+                            />
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
